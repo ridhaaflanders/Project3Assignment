d0d123164ef7443222a3b776a22bdc0b1ee552e2
AthiFukama <athifukama12@gmail.com>
2021-08-30 20:26:05 +0200

    Added review folder

---
+++ review_form/src/main/java/com/ReviewFormApplication/ReviewForm.java
@@ -0,0 +1,71 @@
+package com.ReviewFormApplication;
+
+public class ReviewForm {
+  private String firstname;
+  private String lastname;
+  private String province;
+  private String sentence1;
+  private String sentence2;
+  private String sentence3;
+  private String sentence4;
+  private String sentence5;
+
+
+  public String getFirstName() {
+    return firstname;
+  }
+  public String getLastName() {
+    return lastname;
+  }
+  public String getProvince() {
+    return province;
+  }
+  public String getSentence1() {
+    return sentence1;
+  }
+  public String getSentence2() {
+    return sentence2;
+  }
+  public String getSentence3() {
+    return sentence3;
+  }
+  public String getSentence4() {
+    return sentence4;
+  }
+  public String getSentence5() {
+    return sentence5;
+  }
+
+ 
+
+
+  public void setFirstName(String firstname) {
+    this.firstname = firstname;
+  }
+  public void setLastName(String lastname) {
+    this.lastname = lastname;
+  }
+  public void setProvince(String province) {
+    this.province = province;
+  }
+
+  public void setSentence1(String sentence1) {
+    this.sentence1 = sentence1;
+  }
+  public void setSentence2(String sentence2) {
+    this.sentence2 = sentence2;
+  }
+  public void setSentence3(String sentence3) {
+    this.sentence3 = sentence3;
+  }
+  public void setSentence4(String sentence4) {
+    this.sentence4 = sentence4;
+  }
+  public void setSentence5(String sentence5) {
+    this.sentence5 = sentence5;
+  }
+
+
+}
+    
+
